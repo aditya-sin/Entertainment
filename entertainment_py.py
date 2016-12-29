d99ffe1de920d84343f6e3af3699376d8fcf7c89
Aditya <adi.singhking@gmail.com>
2016-12-29 20:08:08 +0530

    Changing formatting in media.py and entertainment_py.py as per the reviews from Udacity.

--- entertainment_py.py
+++ entertainment_py.py
@@ -11,37 +11,46 @@ Details are - movie name, poster image url, and
 trailer url.
 """
 
-#Creating 9 instances of the class Movie
-toy_story = media.Movie("Toy Story",
-                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg" ,
-                        "https://www.youtube.com/watch?v=vwyZH85NQC4")  
-avatar = media.Movie("Avatar",
-                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
-                     "http://www.youtube.com/watch?v=-9ceBgWV8io")
-dangal = media.Movie("Dangal",
-                     "https://upload.wikimedia.org/wikipedia/en/9/99/Dangal_Poster.jpg",
-                     "https://www.youtube.com/watch?v=x_7YlGv9u1g")
-pk = media.Movie("PK",
-                 "http://t1.gstatic.com/images?q=tbn:ANd9GcQSSaD2bSPOgmgsn4-09dF2l8mHbuvHhJxbkL7rvD1uEpAxKnLX",
-                 "https://www.youtube.com/watch?v=0cziEi0rWmc")
-inception = media.Movie("Inception",
-                        "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg",
-                        "https://www.youtube.com/watch?v=8hP9D6kZseM")
-the_dark_knight = media.Movie("The Dark Knight",
-                              "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg",
-                              "https://www.youtube.com/watch?v=_PZpmTj1Q8Q")
-the_matrix = media.Movie("The Matrix",
-                         "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
-                         "https://www.youtube.com/watch?v=m8e-FF8MsqU")
-terminator_genisys = media.Movie("Terminator Genisys",
-                                 "https://upload.wikimedia.org/wikipedia/en/b/bc/Terminator_Genisys.JPG",
-                                 "https://www.youtube.com/watch?v=62E4FJTwSuc")
-the_avengers = media.Movie("The Avengers",
-                           "https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg",
-                           "https://www.youtube.com/watch?v=eOrNdBpGMv8")
+# Creating 9 instances of the class Movie
+toy_story = media.Movie(
+    "Toy Story",
+    "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg" , # noqa
+    "https://www.youtube.com/watch?v=vwyZH85NQC4") # noqa 
+avatar = media.Movie(
+    "Avatar",
+    "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg", # noqa
+    "http://www.youtube.com/watch?v=-9ceBgWV8io") # noqa
+dangal = media.Movie(
+    "Dangal",
+    "https://upload.wikimedia.org/wikipedia/en/9/99/Dangal_Poster.jpg", # noqa
+    "https://www.youtube.com/watch?v=x_7YlGv9u1g") # noqa
+pk = media.Movie(
+    "PK",
+    "http://t1.gstatic.com/images?q=tbn:ANd9GcQSSaD2bSPOgmgsn4-09dF2l8mHbuvHhJxbkL7rvD1uEpAxKnLX", # noqa
+    "https://www.youtube.com/watch?v=0cziEi0rWmc") # noqa
+inception = media.Movie(
+    "Inception",
+    "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg", # noqa
+    "https://www.youtube.com/watch?v=8hP9D6kZseM") # noqa
+the_dark_knight = media.Movie(
+    "The Dark Knight",
+    "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg", # noqa
+    "https://www.youtube.com/watch?v=_PZpmTj1Q8Q") # noqa
+the_matrix = media.Movie(
+    "The Matrix",
+    "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg", # noqa
+    "https://www.youtube.com/watch?v=m8e-FF8MsqU") # noqa
+terminator_genisys = media.Movie(
+    "Terminator Genisys",
+    "https://upload.wikimedia.org/wikipedia/en/b/bc/Terminator_Genisys.JPG", # noqa
+    "https://www.youtube.com/watch?v=62E4FJTwSuc")# noqa
+the_avengers = media.Movie(
+    "The Avengers",
+    "https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg", # noqa
+    "https://www.youtube.com/watch?v=eOrNdBpGMv8") # noqa
 
-#Creating a list of movies                      
+# Creating a list of movies                      
 movies=[toy_story,avatar,dangal,pk,inception,the_dark_knight,
         the_matrix,terminator_genisys,the_avengers]
-#Opening a web page consting of movies in the list
+# Opening a web page consting of movies in the list
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,20 +1,27 @@
 import webbrowser
 
-class Movie():
-    """Class Movie is used to create multiple
+""" Project 1: Movie Website
+    Date Started:25/12/2016
+    Date Completed: 29/12/2016
+    Submissted by: Aditya Singh
+"""
+
+""" Description: This file creates the Class Movie which
+    is used to create multiple
     instances of movies having following attributes:
     1. Name of the movie - title
     2. URL of the poster - poster_image_url
     3. URL of the trailer - trailer_youtube_url
-    """
+"""
 
-    #Constructor - intialises instances of the class Movie 
+class Movie():
+    
     def __init__(self, movie_title, poster_image,
                  trailer_youtube):
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-    #Function to open trailer of the movie
+    # Function to open trailer of the movie
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
